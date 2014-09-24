19c14e2b30f756c21afd8f0df4beb21aaf8cbad8
Christopher Marshall <thoughtpalettechris@gmail.com>
2014-09-24 16:06:19 -0500

    migrating assets/js from web site, app works, just needs some styling love

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var angMod = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
+angMod.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,3 +17,20 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+
+        $stateProvider
+          .state( "index", {
+              url: "/",
+              templateUrl: "templates/home.html",
+              controller: "MainCtrl"
+          });
+
+        $urlRouterProvider.otherwise('/');
+
+});
